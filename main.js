97b281c1f1ad294c5c65083971ac17e83ae0635d
titohajili <maqatoto963@gmail.com>
2024-01-04 21:40:06 +0400

    Header Section Email Js error (

--- main.js
+++ main.js
@@ -2,3 +2,12 @@
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0);
 })
+//CONTACT BUTTONS(circular tex buttons)
+const textButtons = document.querySelectorAll('.contact-btn');
+
+textButtons.forEach(textButton => {
+    let text = textButton.querySelector('p');
+
+    text.innerHTML = text.innerHTML.split('').map((character, index) => '<span style="transform: rotate( $ { index * 12}deg)"${character}> </span>').join('')
+    console.log(text.innerHTML);
+}) //error
