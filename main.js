2967c899f17f111370ae46c5345218685d778d1b
titohajili <maqatoto963@gmail.com>
2024-01-04 16:38:29 +0400

    Header Section

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 // change nav style on scroll
 window.addEventListener('scroll', () => {
-    document.querySelector('nav').classList.toggle('window-scrolled'), window.scrollY > 0
+    document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0);
 })
