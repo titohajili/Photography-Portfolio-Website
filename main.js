ee96cbfcdf8833599fef82fe6a8d675c7bce390e
titohajili <maqatoto963@gmail.com>
2024-01-08 23:00:10 +0400

    Media Queries Nav

--- main.js
+++ main.js
@@ -33,3 +33,27 @@ var swiper = new Swiper(".mySwiper", {
     }
   });
 
+  //
+
+  const nav = document.querySelector('.nav-links');
+  const openNavBtn = document.querySelector('#nav-toggle-open');
+  const closeNavBtn = document.querySelector('#nav-toggle-close');
+
+  const openNav = () => {
+   nav.style.display = 'flex';
+   openNavBtn.style.display = 'none'
+   closeNavBtn.style.display = 'inline-block'
+  }
+openNavBtn.addEventListener('click', openNav);
+
+const closeNav = () => {
+    nav.style.display = 'none';
+    openNavBtn.style.display = 'inline-block'
+    closeNavBtn.style.display = 'none'
+   }
+closeNavBtn.addEventListener('click', closeNav);
+
+nav.querySelectorAll('li a').forEach(navLink => {
+    navLink.addEventListener('click', closeNav);
+})
+
